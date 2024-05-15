ae91c8aa5a5ed25d6d2471d1f0b651db347cefd7
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-15 00:28:55 -0400

    planos de leitura component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/header"
+import { Heading } from "./components/heading"
 
 export function App() {
   return( 
     <div className="max-w-[1216px] mx-auto py-2 flex flex-col gap-5 items-center">
       <Header />
+      <Heading />
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,23 @@
 import telmaLogo from "../assets/svg/Logo TELMA BARCELLOS matriz.svg"
 import shopIcon from "../assets/icons/icone compras site modacad SVG.svg"
 import menuIcon from "../assets/icons/menu.svg"
+import pesquisaicon from "../assets/icons/pesquisa.svg"
 
 export function Header() {
     return (
-        <div className="flex items-center gap-5 py-2">
-            <img className="h-10" src={telmaLogo}/>
+        <div className="flex items-center gap-5 border-b-2 border-zinc-950 w-10/12">
+            <img className="h-14 " src={telmaLogo}/>
 
             <nav className="flex items-center">
-                <a href="" className="font-medium text-sm border-2 border-r-0 border-zinc-950 w-50 h-20 p-6 ">Meu Perfil</a>
-                <a href="" className="font-medium text-sm border-2 border-r-0 border-zinc-950 w-50 h-20 p-6 ">Criar Perfil</a>
-                <a href="" className="font-medium text-sm border-2 border-r-0 border-zinc-950 w-50 h-20 p-6 ">
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-b-0 border-zinc-950 w-50 h-20 px-10 py-7 ">Meu Perfil</a>
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-b-0 border-zinc-950 w-50 h-20 px-10 py-7 ">Criar Perfil</a>
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-b-0 border-zinc-950 w-50 h-20 px-6 py-4 ">
                     <img src={shopIcon} alt=""  className="h-10" />
                 </a>
-                <a href="" className="font-medium text-sm border-2 border-zinc-950 w-50 h-20 p-6 ">
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-b-0 border-zinc-950 w-50 h-20 px-6 py-7 ">
+                    <img src={pesquisaicon} alt=""  className="h-5" />
+                </a>
+                <a href="" className="font-medium text-sm border-2 border-r-2 border-b-0 border-zinc-950 w-50 h-20 px-6 py-8 ">
                     <img src={menuIcon} />
                 </a>
             </nav>
