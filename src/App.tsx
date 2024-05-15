9b8805c0e384ee4ca1bf6add849dcfed5b36e7d0
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-14 21:32:40 -0400

    header css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Header } from "./components/header"
 
-function App() {
-  return (
-      <div>
-        <Header />
-      </div>
+export function App() {
+  return( 
+    <div className="max-w-[1216px] mx-auto py-2 flex flex-col gap-5 items-center">
+      <Header />
+    </div>
   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,21 @@
 import telmaLogo from "../assets/svg/Logo TELMA BARCELLOS matriz.svg"
+import shopIcon from "../assets/icons/icone compras site modacad SVG.svg"
+import menuIcon from "../assets/icons/menu.svg"
 
 export function Header() {
     return (
         <div className="flex items-center gap-5 py-2">
-            <img src={telmaLogo}/>
+            <img className="h-10" src={telmaLogo}/>
 
-            <nav className="flex items-center gap-5">
-                <a href="" className="font-medium text-sm text-neutral-500">Meu Perfil</a>
-                <a href="" className="font-medium text-sm">Criar Perfil</a>
+            <nav className="flex items-center">
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-zinc-950 w-50 h-20 p-6 ">Meu Perfil</a>
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-zinc-950 w-50 h-20 p-6 ">Criar Perfil</a>
+                <a href="" className="font-medium text-sm border-2 border-r-0 border-zinc-950 w-50 h-20 p-6 ">
+                    <img src={shopIcon} alt=""  className="h-10" />
+                </a>
+                <a href="" className="font-medium text-sm border-2 border-zinc-950 w-50 h-20 p-6 ">
+                    <img src={menuIcon} />
+                </a>
             </nav>
         </div>
     )
