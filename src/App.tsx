329d68f032b16249f3ba97ad16e81df3c5bad5fa
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-15 00:46:10 -0400

    reading box styling init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/header"
-import { Heading } from "./components/heading"
+import { ReadingBox } from "./components/reagindBox"
 
 export function App() {
   return( 
     <div className="max-w-[1216px] mx-auto py-2 flex flex-col gap-5 items-center">
       <Header />
-      <Heading />
+      <ReadingBox />
     </div>
   )
 }
