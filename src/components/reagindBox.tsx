6d1dbb08165e86d7ab2efdb63f9f6ad3986339ca
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-17 09:51:00 -0400

    reading box#2

--- src/components/reagindBox.tsx
+++ src/components/reagindBox.tsx
@@ -3,25 +3,27 @@ import setaCurvaIcon from "../assets/icons/seta-curva.svg"
 export function ReadingBox() {
 
     return (
-        <div className="flex items-center gap-4 border-2 border-zinc-950 w-10/12 p-10 py-5" >
+        <div className="flex items-center gap-24 border-2 border-zinc-950 w-10/12 p-10 py-5" >
 
             <div className="grid gap-2">
                 <p className="text-5xl">Planos de leitura</p>
                 <span>Plano basico de leitura</span>
             </div>
 
-            <img src={setaCurvaIcon} alt="" />
+            <img src={setaCurvaIcon} className="w-10" />
 
             <div className="grid gap-4">
             <input type="email" placeholder="Coloque seu e-mail aqui..." className="mt-1  w-full px-3 py-2 bg-transparent 
-                border-b-2 border-slate-900 text-sm shadow-sm placeholder-slate-700
+                border-b-2 border-slate-900 text-sm shadow-sm placeholder-slate-700 text-center
                 focus:outline-none
                 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                 invalid:border-pink-500 invalid:text-pink-600
                 focus:invalid:border-pink-500 focus:invalid:ring-pink-500
             "/>
-                <button type="submit" className=" grid item-center border-2 border-zinc-900 p-5">
+                <button type="submit" className="item-center border-2 border-zinc-900 p-5">
                     Criar Perfil
+                    <div className="w-48 h-7 bg-yellow-400/65 bottom-5 relative
+                    "></div>
                 </button>
             </div>
         </div>
