c1fa71f24a540da1d5e8c3dd2aeef2937c39ac02
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-19 08:30:48 -0400

    subjects box: styling#01

--- src/components/subjects.tsx
+++ src/components/subjects.tsx
@@ -3,29 +3,30 @@
 export function Subjects() {
 
     return (
-        <main className="flex items-center gap-4" >
-            <div className="">
-                <span>ASSUNTOS</span>
+        <main className="flex items-center gap-5 p-10" >
+            <div className="transform: -rotate-90 ">
+                <p className="font-medium text-xs">ASSUNTOS</p>
             </div >
 
-            <div className="grid grid-cols-4" >
-            <p>moldesModacad</p>
-            <p>Tendências</p>
-            <p>Cores e Moda</p>
-            <p>Tecidos e Materiais</p>
+            <div className=" gap-4 w-full text-4xl items-center align-middle text-left" >
 
-            <p>Alfaiataria</p>
-            <p>Moda afro</p>
-            <p>Moda indígena</p>
+                <a href="" className="font-bold py-5 left-0">moldesModacad</a>
+                <a href="" className=" px-5">Tendências</a>
+                <a href="" className=" px-5">Cores e Moda</a>
+                <a href="" className=" px-5">Tecidos e Materiais</a>
 
-            <p>Estamparia</p>
-            <p>Tecnologia</p>
-            <p>Sustentabilidade</p>
+                <a href="" className=" px-5">Alfaiataria</a>
+                <a href="" className=" px-5">Moda afro</a>
+                <a href="" className="">Moda indígena</a>
+                <br />
+                <a href="" className="">Estamparia</a>
 
-            <p>História da Moda</p>
-            <p>Moda e crises</p>
+                <a href="" className=" px-5">Tecnologia</a>
+                <a href="" className=" px-5">Sustentabilidade</a>
+                <a href="" className=" px-5">História da Moda</a>
+                <a href="" className=" px-5">Moda e crises</a>
 
-            <p>pílulasModacad</p>
+                <a href="" className="font-bold p-5">pílulasModacad</a>
 
             </div>
         </main>
