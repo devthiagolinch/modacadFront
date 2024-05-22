0391bd962f712c15e58a57073dfccd1ebb2e997b
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-22 15:49:31 -0400

    header fix distribuition#01

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,13 @@ import pesquisaicon from "../assets/icons/pesquisa.svg"
 
 export function Header() {
     return (
-        <div className=" flex items-center justify-center left-0 w-full fixed border-b-2 border-zinc-950 bg-neutral-200">
-            <div className="flex items-center gap-10 relative ">
+        <div className=" flex items-center  left-0 w-full fixed border-b-2 border-zinc-950 bg-neutral-200">
+            <div className="flex w-full items-center justify-between relative">
 
-                <img className="h-10" src={telmaLogo}/>
+                <div className=" flex-1 bg-green-500 justify-items-center" >
+                    <img className="h-10" src={telmaLogo}/>
+                </div>
+                
 
                 <nav className="flex items-center">
                     <a href="" className="font-medium text-sm border-2 border-r-0 border-b-0 border-t-0 border-zinc-950 w-50 h-20 px-10 py-7 ">Meu Perfil</a>
