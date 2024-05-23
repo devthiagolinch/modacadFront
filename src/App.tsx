e697f324a30dcc5af5c812d979205570b51afeef
Thiago Linch <dev.thiagolinch@gmail.com>
2024-05-23 10:04:18 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "./components/footer"
 import { Header } from "./components/header"
+import { LastPost } from "./components/last-post"
 import { ReadingBox } from "./components/reagindBox"
 import { Subjects } from "./components/subjects"
 
@@ -13,6 +14,7 @@ export function App() {
             </div>
 
             <ReadingBox />
+            <LastPost />
             <Subjects />
             <div className="h-96">
     
---
+++ src/components/last-post.tsx
@@ -0,0 +1,27 @@
+
+
+export function LastPost() {
+
+    return(
+        <div className="flex w-full h-96 p-10 bg-yellow-400">
+
+            <div className="h-full items-center justify-center align-middle">
+                <p className="transform: -rotate-90">Último Texo</p>
+            </div>
+            <div className="flex-1 bg-slate-200">
+                <span>
+                    sfkljfklasjfklaj <br />
+                    asflkasjfksaljf <br />
+                    asflasfljk <br />
+
+
+                </span>
+            </div>
+
+            <div className="flex-1 bg-orange-500">
+
+            </div>
+            
+        </div>
+    )
+}
