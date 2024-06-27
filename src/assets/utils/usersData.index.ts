778de67857549995638636343056bc6ae92e09d8
Thiago Linchin <dev.thiagolinch@gmail.com>
2024-06-27 07:08:59 -0400

    dash#01

---
+++ src/assets/utils/usersData.index.ts
@@ -0,0 +1,8 @@
+import telmaAvatar from "../imgs/telma.jpeg"
+
+export const UsersData = {
+    id: "1",
+    name: "Thelma Barcellos",
+    email: "telmabarcelos@gmail.com",
+    authorAvatar: telmaAvatar,
+}
