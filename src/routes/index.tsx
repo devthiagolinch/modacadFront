cc2e2a40d967bf944979174db26804cce14b8737
Jeú Chaves <jeuchaveslima@gmail.com>
2024-09-07 15:22:26 -0400

    fix: corrects authentication verification logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, RouteProps, Routes } from "react-router-dom"
 
-import { LOCAL_STORAGE_KEY__ACCESS_TOKEN } from "../lib/auth";
+import { useUser } from "../shared/contexts/UserContext";
 
 import { Home } from "../pages/Home";
 import { PilulasMCD } from "../pages/PilulasMCD";
@@ -17,10 +17,14 @@ import { UpdatePost } from "../pages/dashboard/AtualizarPost";
 
 type TPrivateRouteProps = RouteProps & {
     element: React.ReactNode;
-  };
-  
+};
+
+export const AppRoutes = () => {
+
+  const { user } = useUser();
+
   const PrivateRoute: React.FC<TPrivateRouteProps> = ({ element }) => {
-    const isAuthenticated = !!localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
+    const isAuthenticated = !!user;
   
     return isAuthenticated ? (
       element
@@ -29,24 +33,23 @@ type TPrivateRouteProps = RouteProps & {
     )
   }
 
-export const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/maislidos" element={<TextosMaisLidos/>} />
-            <Route path="/pilulas" element={<PilulasPublicadas/>} />
-            <Route path="/pilulas/:pilulaId" element={<PilulasMCD />} />
-            <Route path="/textosmodacad" element={<PublishText/>} />
-            <Route path="/texto/:textId" element={<TextosModacad/>} />
-            <Route path="/planos" element={<PlanosMDC/>} />
-            
-            <Route path="/dashboard/new-post" element={<PrivateRoute element={<NewPost/>} />} />
-            <Route path="dashboard/profile/post-editor/:id" element={<PrivateRoute element={<UpdatePost/>} />} />
-            <Route path="/dashboard/profile" element={<PrivateRoute element={<Dashboard/>} />} />
-            
-            <Route path="/admin-login" element={<AdminLoginPage />} />
-            
-            <Route path="/blank" element={<BlanckPage/>} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/maislidos" element={<TextosMaisLidos/>} />
+      <Route path="/pilulas" element={<PilulasPublicadas/>} />
+      <Route path="/pilulas/:pilulaId" element={<PilulasMCD />} />
+      <Route path="/textosmodacad" element={<PublishText/>} />
+      <Route path="/texto/:textId" element={<TextosModacad/>} />
+      <Route path="/planos" element={<PlanosMDC/>} />
+      
+      <Route path="/dashboard/new-post" element={<PrivateRoute element={<NewPost/>} />} />
+      <Route path="dashboard/profile/post-editor/:id" element={<PrivateRoute element={<UpdatePost/>} />} />
+      <Route path="/dashboard/profile" element={<PrivateRoute element={<Dashboard/>} />} />
+      
+      <Route path="/admin-login" element={<AdminLoginPage />} />
+      
+      <Route path="/blank" element={<BlanckPage/>} />
+    </Routes>
+  )
 }
