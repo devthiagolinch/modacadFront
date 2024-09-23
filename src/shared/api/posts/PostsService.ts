12c94e40365c24742ab0962b9a48e31bae23b327
Jeú Chaves <jeuchaveslima@gmail.com>
2024-09-23 17:16:46 -0400

    feat: adiciona campo para adição de imagem e opção de adicionar um novo arquivo

--- src/shared/api/posts/PostsService.ts
+++ src/shared/api/posts/PostsService.ts
@@ -1,18 +1,26 @@
 import { Status, PostType, Visibility } from '../../services/postOptions';
 import { api } from '../../services/axios';
+import { IUserData } from '../users/UserServices';
 
 export interface IPostData {
-  id: string;
-  title: string;
-  description: string;
-  type: PostType;
+  admins: IUserData;
   content: string;
-  admin: string;
-  tags: string[] | string | null;
-  subjects: string[] | string | null;
+  created_at: Date;
+  description: string;
+  feature_image: string | null;
+  id: string;
+  published_at: Date;
   status: Status;
+  tags: string[];
+  subjects: string[];
+  title: string;
+  type: PostType;
   visibility: Visibility;
+  updated_at: Date;
 }
+
+export interface IPostSave
+  extends Omit<IPostData, 'id' | 'admins' | 'created_at' | 'published_at' | 'updated_at' | 'feature_image'> {}
 
 const getAll = async (type: 'pilula' | 'texto', statusId?: string, authorId?: string): Promise<IPostData[] | Error> => {
   try {
@@ -30,7 +38,7 @@ const getAll = async (type: 'pilula' | 'texto', statusId?: string, authorId?: st
   }
 };
 
-const create = async (post: Omit<IPostData, 'id' | 'admin'>): Promise<IPostData | Error> => {
+const create = async (post: IPostSave): Promise<IPostData | Error> => {
   try {
     const { data } = await api.post<IPostData>('/post', post);
 
@@ -52,11 +60,32 @@ const getById = async (postId: string): Promise<IPostData | Error> => {
   }
 };
 
-const updateById = async (postId: string, post: Omit<IPostData, 'id' | 'admin'>): Promise<void | Error> => {
+const updateById = async (postId: string, post: IPostSave): Promise<void | Error> => {
   try {
     await api.put<IPostData>(`/post/${postId}`, post);
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido ao atualizar o registro';
+    return new Error(errorMessage);
+  }
+};
+
+const uploadFeatureImage = async (postId: string, file: File): Promise<string | Error> => {
+  try {
+    const formData = new FormData();
+    formData.append('images', file);
+
+    const urlRelativa = `/post/images/feature-image/${postId}`;
+    const { data } = await api.post<string>(urlRelativa, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    if (data) {
+      return data;
+    }
+    return new Error('Erro ao fazer upload da imagem');
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido ao fazer upload da imagem';
     return new Error(errorMessage);
   }
 };
@@ -66,4 +95,5 @@ export const PostsService = {
   create,
   getById,
   updateById,
+  uploadFeatureImage,
 };
---
+++ src/shared/api/users/UserServices.ts
@@ -0,0 +1,13 @@
+export interface IUserData {
+  avatar: string | null;
+  cellphone: string | null;
+  created_at: Date;
+  email: string;
+  id: string;
+  name: string;
+  password: string;
+  plan: string | null;
+  role: string;
+  status: string;
+  updated_at: Date;
+}
