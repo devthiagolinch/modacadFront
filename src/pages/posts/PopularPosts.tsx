3d562d70e23be5471cc79486b8bb1b1483e03501
Jeú Chaves <jeuchaveslima@gmail.com>
2024-09-26 22:08:03 -0400

    feat: Corrige página de post populares

--- src/pages/posts/PopularPosts.tsx
+++ src/pages/posts/PopularPosts.tsx
@@ -1,13 +1,24 @@
-import { ServiceData } from '../../assets/utils/constants.index';
 import { Header } from '../../shared/components/header';
 import { TextoMocadCard } from '../../shared/components/cards/textoModacadCard';
 import { Footer } from '../../shared/components/footer';
 import { Link } from 'react-router-dom';
 import { ReadingBox } from '../../shared/components/reagindBox';
 import { Button } from '../../shared/components/Button';
+import { IPostData, PostsService } from '../../shared/api/posts/PostsService';
+import { useEffect, useState } from 'react';
 
 export function PopularPosts() {
-  const data = ServiceData;
+  const [posts, setPosts] = useState<IPostData[]>();
+
+  useEffect(() => {
+    PostsService.getAll('texto').then((response) => {
+      if (response instanceof Error) {
+        console.error(response.message);
+        return;
+      }
+      setPosts(response);
+    });
+  });
 
   return (
     <div className="">
@@ -24,17 +35,12 @@ export function PopularPosts() {
         </div>
 
         <div className="lg:grid lg:grid-cols-2 mt-5 flex flex-col justify-center items-center lg:px-20">
-          {data.map((texto) => (
-            <Link to={`/postsmodacad/${texto.id}`}>
-              <TextoMocadCard
-                id={texto.id}
-                banner={texto.backgroundImage}
-                title={texto.title}
-                description={texto.description}
-                tags={texto.tag}
-              />
-            </Link>
-          ))}
+          {posts &&
+            posts.map((post) => (
+              <Link to={`/posts/${post.id}`} key={post.id}>
+                <TextoMocadCard post={post} />
+              </Link>
+            ))}
         </div>
         <div className="lg:mb-[80px] lg:mt-[60px] mt-[25px] mb-[50px] justify-center items-center flex">
           <Button title={'Carregar mais'} active={false} />
