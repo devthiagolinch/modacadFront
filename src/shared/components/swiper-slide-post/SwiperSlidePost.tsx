3a0b27bfd5202857496d409571e8bdc1cfc885df
Jeú Chaves <jeuchaveslima@gmail.com>
2024-11-04 20:48:47 -0400

    feat: Atualiza estilos e estrutura do componente SwiperSlidePost

--- src/shared/components/swiper-slide-post/SwiperSlidePost.tsx
+++ src/shared/components/swiper-slide-post/SwiperSlidePost.tsx
@@ -8,10 +8,13 @@ interface ISwiperSlidePost {
 export const SwiperSlidePost: React.FC<ISwiperSlidePost> = ({ post }) => {
   return (
     <div className={`border-r border-gray-950 font-montserrat font-medium h-full`}>
+      {/* Imagem da publicação */}
       <div className="border-b-[1px] border-[#202020]">
-        <img src={post.feature_image ?? ''} alt={`Imagem do ${post.title}`} className="h-[300px] object-cover w-full" />
+        <img src={post.feature_image ?? ''} alt={`Imagem do ${post.title}`} className="h-[400px] object-cover w-full" />
       </div>
-      <div className="p-[40px] flex-grow flex flex-col justify-between">
+      {/* Conteúdo da publicação */}
+      <div className="flex flex-col gap-4 p-8">
+        {/* Assuntos */}
         <ul className="flex flex-col">
           {post?.subjects && post.subjects.length > 0 && (
             <li className="font-light text-xl">
@@ -22,10 +25,10 @@ export const SwiperSlidePost: React.FC<ISwiperSlidePost> = ({ post }) => {
             </li>
           )}
         </ul>
-        <h1 className="font-butler_regular text-[25px] leading-[30px] mb-[13px] mt-[12px]">{post.title}</h1>
-        <p className="font-montserratRegular text-[17px] leading-[20px] mb-[10px] h-[60px] overflow-hidden">
-          {post.description}
-        </p>
+        {/* Título */}
+        <h1 className="font-butler text-4xl font-light">{post.title}</h1>
+        {/* Descrição */}
+        <p className="font-light text-gray-800">{post.description}</p>
       </div>
     </div>
   );
