a996d0474313285bd6066d79fb9fcd57cc514d07
Jeú Chaves <jeuchaveslima@gmail.com>
2024-11-04 20:58:38 -0400

    feat: Adiciona navegação personalizada ao componente SwiperPosts

--- src/shared/components/posts/swiper-posts/SwiperPosts.tsx
+++ src/shared/components/posts/swiper-posts/SwiperPosts.tsx
@@ -6,9 +6,9 @@ import { IPostData } from '../../../../shared/api/posts/PostsService';
 import { TPostsType } from 'src/shared/services/postOptions';
 
 import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/free-mode';
 import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/24/outline';
+import { Navigation } from 'swiper/modules';
+import { useRef } from 'react';
 
 interface ISwiperPosts {
   posts: IPostData[];
@@ -17,6 +17,10 @@ interface ISwiperPosts {
 }
 
 export const SwiperPosts: React.FC<ISwiperPosts> = ({ posts, postType, title }) => {
+  // Criar referências para os botões de navegação
+  const prevRef = useRef<HTMLDivElement>(null);
+  const nextRef = useRef<HTMLDivElement>(null);
+
   return (
     <div>
       <div className="grid grid-cols-12 gap-4 font-montserrat">
@@ -24,7 +28,25 @@ export const SwiperPosts: React.FC<ISwiperPosts> = ({ posts, postType, title }) 
           <h2 className="transform -rotate-90 text-2xl text-nowrap font-light text-gray-700">{title}</h2>
         </div>
         <div className="col-span-11 border-x border-t border-gray-950">
-          <Swiper slidesPerView={Math.min(posts.length, 2)}>
+          <Swiper
+            slidesPerView={Math.min(posts.length, 2)}
+            modules={[Navigation]}
+            loop={true}
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            onBeforeInit={(swiper) => {
+              // Atribuir as referências dos botões de navegação ao Swiper
+              if (typeof swiper.params.navigation !== 'boolean') {
+                const navigation = swiper.params.navigation;
+                if (navigation) {
+                  navigation.prevEl = prevRef.current;
+                  navigation.nextEl = nextRef.current;
+                }
+              }
+            }}
+          >
             {posts.map((post) => (
               <SwiperSlide key={post.id} style={{ height: 'auto' }}>
                 <Link to={`/${postType === 'texto' ? 'posts' : 'pilulas'}/${post.id}`}>
@@ -37,10 +59,16 @@ export const SwiperPosts: React.FC<ISwiperPosts> = ({ posts, postType, title }) 
       </div>
       {/* Menu de navegação */}
       <div className="swiper-navigation flex">
-        <div className="swiper-button-prev flex grow justify-center hover:bg-primary cursor-pointer py-6 border border-gray-950">
+        <div
+          ref={prevRef}
+          className="swiper-button-prev flex grow justify-center hover:bg-primary cursor-pointer py-6 border border-gray-950"
+        >
           <ArrowLongLeftIcon className="size-12" />
         </div>
-        <div className="swiper-button-next flex grow justify-center hover:bg-primary cursor-pointer py-6 border-y border-r border-gray-950">
+        <div
+          ref={nextRef}
+          className="swiper-button-next flex grow justify-center hover:bg-primary cursor-pointer py-6 border-y border-r border-gray-950"
+        >
           <ArrowLongRightIcon className="size-12" />
         </div>
       </div>
