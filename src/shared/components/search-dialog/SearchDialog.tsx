6b7c28c91fc895b97a635db6dc614ef3d756b494
Thiago Linchin <79920937+devthiagolinch@users.noreply.github.com>
2024-12-13 21:09:56 -0400

    Update SearchDialog.tsx

--- src/shared/components/search-dialog/SearchDialog.tsx
+++ src/shared/components/search-dialog/SearchDialog.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import { PostsService } from '../../../shared/api/posts/PostsService';
 import { Link } from 'react-router-dom';
 
