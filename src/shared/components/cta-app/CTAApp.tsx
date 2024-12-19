55f9db391bb2088f7683cb882c57fffed6e81fac
Jeú Chaves <jeuchaveslima@gmail.com>
2024-12-18 22:21:38 -0400

    FEAT: melhorias no layout do componente CTAApp | ajuste na estrutura e responsividade

--- src/shared/components/cta-app/CTAApp.tsx
+++ src/shared/components/cta-app/CTAApp.tsx
@@ -4,20 +4,22 @@ import logotipo from '../../../assets/logos/modacad-logotipo.svg';
 
 export const CTAApp = () => {
   return (
-    <div className="container mx-auto flex flex-col md:flex-row gap-4 items-center justify-between font-montserrat p-4">
-      <div>
-        <p className="text-7xl font-butler font-light">
+    <div className="container mx-auto font-montserrat px-4 py-16 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="flex flex-col justify-center items-center md:items-start">
+        <p className="text-6xl md:text-7xl font-butler font-light">
           moldes
           <br />
           Modacad
         </p>
       </div>
-      <img src={setaCurvaIcon} className="transform -scale-x-90 rotate-[216deg] md:scale-x-100 md:rotate-0" />
-      <div className="flex gap-2 items-center">
+      <div className="flex flex-col justify-center items-center">
+        <img src={setaCurvaIcon} className="transform -scale-x-90 rotate-[216deg] md:scale-x-100 md:rotate-0" />
+      </div>
+      <div className="flex justify-end items-center gap-4 justify-center md:justify-start flex-wrap">
         <div>
           <p className="font-light text-2xl mb-2">Como funciona</p>
           <a href="https://www.modacad.com.br/#como-funciona" target="_blank" rel="noopener noreferrer">
-            <div className="border-2 border-gray-950 p-4 hover:bg-primary">
+            <div className="border border-gray-950 p-4 hover:bg-primary">
               <img src={logoEstendida} className="object-contain w-full h-full" />
             </div>
           </a>
@@ -25,7 +27,7 @@ export const CTAApp = () => {
         <div>
           <p className="font-light text-2xl mb-2">Conheça o app</p>
           <a href="https://app.modacad.com.br/" target="_blank" rel="noopener noreferrer">
-            <div className="border-2 border-gray-950 p-4 hover:bg-primary">
+            <div className="border border-gray-950 p-4 hover:bg-primary">
               <img src={logotipo} className="object-contain w-full h-full" />
             </div>
           </a>
