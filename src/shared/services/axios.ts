87bb4cac1f88f53c1ebe0785eb3732aaf7e6097e
Jeú Chaves <jeuchaveslima@gmail.com>
2025-01-31 21:52:24 -0400

    adiciona interceptador de requisições ao axios para incluir token de autenticação

--- src/shared/services/axios.ts
+++ src/shared/services/axios.ts
@@ -1,19 +1,25 @@
-import axios from 'axios'
-import { LOCAL_STORAGE_KEY__ACCESS_TOKEN } from './auth'
+import axios from 'axios';
+import { LOCAL_STORAGE_KEY__ACCESS_TOKEN } from './auth';
 
 const api = axios.create({
-    baseURL: "https://api-modacad-72uqj.ondigitalocean.app/",
-    headers: {
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN) || '""')}`,
-        "Content-type": "application/json",
+  baseURL: 'https://api-modacad-72uqj.ondigitalocean.app/',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
     }
 
-    // salvar o token no localStorage,
-    // function de login e logout
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-    // service de authentication - pra salver o login do token e remover o token
-    // configurar no axios que assim que fizer a requisicao tem alguma coisa no localstorage, se tiver algo ele envia na requisicao
-    // bater
-})
-
-export  {api}
+export { api };
