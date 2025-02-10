40816826135f54605cd10e42a3541d777a4ed62b
Jeú Chaves <jeuchaveslima@gmail.com>
2025-02-10 17:16:48 -0400

    atualiza links das redes sociais no componente CTARedesSociais e corrige nome da variável do ícone do Instagram

--- src/shared/components/cta/CTARedesSociais.tsx
+++ src/shared/components/cta/CTARedesSociais.tsx
@@ -1,18 +1,18 @@
 import pinterestIcon from '../../../assets/icons/social  media/pinterest.svg';
 import linkedinIcon from '../../../assets/icons/social  media/linkedin.svg';
 import facebookicon from '../../../assets/icons/social  media/facebook.svg';
-import instaIcon from '../../../assets/icons/social  media/instagram.svg';
+import instagramIcon from '../../../assets/icons/social  media/instagram.svg';
 import youtubeIcon from '../../../assets/icons/social  media/youtube.svg';
 import tiktokIcon from '../../../assets/icons/social  media/tiktok.svg';
 import setaCurvaIcon from '../../../assets/icons/seta-curva.svg';
 
 const socialMedia = [
-  { icon: linkedinIcon, link: 'https://www.linkedin.com/' },
-  { icon: pinterestIcon, link: 'https://www.pinterest.com/' },
-  { icon: instaIcon, link: 'https://www.instagram.com/' },
-  { icon: tiktokIcon, link: 'https://www.tiktok.com/' },
-  { icon: youtubeIcon, link: 'https://www.youtube.com/' },
-  { icon: facebookicon, link: 'https://www.facebook.com/' },
+  { icon: linkedinIcon, link: 'https://www.linkedin.com/company/moldesmodacad/' },
+  { icon: pinterestIcon, link: 'https://br.pinterest.com/moldesModacad/' },
+  { icon: instagramIcon, link: 'https://www.instagram.com/appmodacad/' },
+  { icon: tiktokIcon, link: 'https://www.tiktok.com/@modacadapp' },
+  { icon: youtubeIcon, link: 'https://youtube.com/@modacad7069?si=oDqecFR-Gc6LLgvv' },
+  { icon: facebookicon, link: 'https://www.facebook.com/moldesModacad/' },
 ];
 
 export const CTARedesSociais = () => {
@@ -28,12 +28,15 @@ export const CTARedesSociais = () => {
       </div>
       <div className="grid grid-cols-3 gap-x-4 gap-y-4">
         {socialMedia.map((social, index) => (
-          <div
-            className="border border-gray-950 p-4 flex items-center justify-center aspect-w-1 aspect-h-1 hover:bg-primary cursor-pointer"
+          <a
+            href={social.link}
+            target="_blank"
+            rel="noopener noreferrer"
             key={index}
+            className="border border-gray-950 p-4 flex items-center justify-center aspect-w-1 aspect-h-1 hover:bg-primary cursor-pointer"
           >
             <img src={social.icon} className="object-contain w-1/2 h-1/2" />
-          </div>
+          </a>
         ))}
       </div>
     </div>
