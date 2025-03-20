07e9c28e7a8772ae11197b8c1b244b4f572b7038
Thiago Linchin <dev.thiagolinch@gmail.com>
2025-03-20 16:14:26 -0400

    feat: implement pagination and sorting for members list in admin panel

--- src/pages/admin/members/PageMembers.tsx
+++ src/pages/admin/members/PageMembers.tsx
@@ -2,42 +2,64 @@ import { useEffect, useState } from 'react';
 import { LayoutDashboard } from '../../../shared/layouts';
 import { IUserData, UsersService } from '../../../shared/api/users/UserServices';
 import { getRoleClass } from '../../../shared/hook/getRoleClass';
+import { DropdownMenu } from '../../../shared/components/ui/dropdown-menu/DropdownMenu';
+import { useSearchParams } from 'react-router-dom';
 
 export const PageMembers = () => {
   const [members, setMembers] = useState<IUserData[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const [totalMembros, setTotalMembros] = useState(0);
   const [totalAssinantes, setTotalAssinantes] = useState(0);
   const [totalExAssinantes, setTotalExAssinantes] = useState(0);
 
-  const fetchMembers = async (page: number) => {
-    UsersService.getAll({ page }).then((response) => {
-      if (response instanceof Error) {
-        console.error(response);
-        return;
-      }
-      setMembers(response.users);
-      setCurrentPage(response.currentPage);
-      setTotalPages(response.totalPages);
-
-      setTotalAssinantes(response.totalAssinantes);
-      setTotalMembros(response.totalMembros);
-      setTotalExAssinantes(response.totalExAssinantes);
-    });
-  };
-
   useEffect(() => {
-    fetchMembers(1);
-  }, []);
+    setLoading(true);
+    const fetchMembers = async () => {
+      UsersService.getAll({ 
+        page: Number(searchParams.get('page')) || 1,
+        order: searchParams.get('order') as 'asc' | 'desc'}).then((response) => {
+        if (response instanceof Error) {
+          console.error(response);
+          return;
+        }
+        setMembers(response.users);
+        setCurrentPage(response.currentPage);
+        setTotalPages(response.totalPages);
+  
+        setTotalAssinantes(response.totalAssinantes);
+        setTotalMembros(response.totalMembros);
+        setTotalExAssinantes(response.totalExAssinantes);
 
-  const handlePrevious = () => {
-    if (currentPage > 1) fetchMembers(currentPage - 1);
+        setLoading(false);
+      });
+    };
+
+    fetchMembers();
+  }, [searchParams]);
+
+  const orderOptions = [
+    { name: 'Mais recentes', key: 'desc' },
+    { name: 'Mais antigos', key: 'asc' },
+  ];
+  function handleOrderChange(key: string): void {
+    updateOrderParam(key as 'asc' | 'desc');
+  }
+
+  const updateOrderParam = (order: 'asc' | 'desc') => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set('order', order);
+    setSearchParams(newSearchParams);
   };
 
-  const handleNext = () => {
-    if (currentPage < totalPages) fetchMembers(currentPage + 1);
+  const updatePageParam = (page: number) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set('page', page.toString());
+    setSearchParams(newSearchParams);
   };
 
   return (
@@ -45,9 +67,13 @@ export const PageMembers = () => {
       <div className="flex justify-between">
         <div className="flex gap-2 items-center">
           <button className="bg-bgBtn text-white font-medium px-4 py-2">EXPORTAR</button>
-          <button className="bg-gray-950 px-4 py-2 text-white">filtro 1</button>
-          <button className="bg-gray-950 px-4 py-2 text-white">filtro 2</button>
+          {/* Filtros */}
+                <div className="flex gap-2 my-4">
+                  {/* <DropdownMenu textButton="Plano" items={statusesOptions} onSelect={handleStatusChange} /> */}
+                  <DropdownMenu textButton="Ordenação" items={orderOptions} onSelect={handleOrderChange} />
+                </div>
         </div>
+        {/* Tabela */}
         <div className="flex gap-2">
           <table>
             <thead>
@@ -73,7 +99,12 @@ export const PageMembers = () => {
           </table>
         </div>
       </div>
-      <div className="bg-white mt-4 border border-gray-300 font-montserrat font-medium overflow-y-auto">
+      { loading ? (
+        <div className="flex justify-center items-center">
+          <div className="loader">Carregando...</div>
+        </div>
+      ) : (
+        <div className="bg-white mt-4 border border-gray-300 font-montserrat font-medium overflow-y-auto">
         <ul>
           {members.map((member) => (
             <li
@@ -87,8 +118,8 @@ export const PageMembers = () => {
         </ul>
         <div className="flex justify-between items-center border-t border-gray-300 p-4">
           <button
-            onClick={handlePrevious}
-            disabled={currentPage === 1}
+            onClick={() => updatePageParam(currentPage - 1)}
+            disabled={currentPage <= 1}
             className={`px-4 py-2 text-sm ${currentPage === 1 ? 'bg-gray-200 cursor-not-allowed' : 'bg-bgBtn text-white'}`}
           >
             Anterior
@@ -97,14 +128,16 @@ export const PageMembers = () => {
             Página {currentPage} de {totalPages}
           </p>
           <button
-            onClick={handleNext}
-            disabled={currentPage === totalPages}
+            onClick={() => updatePageParam(currentPage + 1)}
+            disabled={currentPage >= totalPages}
             className={`px-4 py-2 text-sm ${currentPage === totalPages ? 'bg-gray-200 cursor-not-allowed' : 'bg-bgBtn text-white'}`}
           >
             Próximo
           </button>
         </div>
       </div>
+      ) }
+
     </LayoutDashboard>
   );
 };
